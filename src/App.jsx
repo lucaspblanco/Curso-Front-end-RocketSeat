341e20f46c548fdac04ea32dab84b4c9174cd09c
lucasfallen <lucasblanco44.com@gmail.com>
2024-09-16 10:46:05 -0300

    Estilização do Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Header } from './components/Header'
 import { Post } from './components/Post'
 import styles from './App.module.css'
-import './global.css';
 import { Sidebar } from './components/Sidebar';
-
+import './global.css';
 function App() {
 
   return (
